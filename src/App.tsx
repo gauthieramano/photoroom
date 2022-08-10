52829a05af4c38a86129f8a4bf4ca90052826de0
Gauthier Amano <gauthier.net@gmail.com>
2022-08-10 20:14:39 +0200

    refactor(app): ♻️ use `useImageUpload` hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,18 @@
-import loadImage, { LoadImageResult } from "blueimp-load-image";
-import { ChangeEvent, useCallback, useState } from "react";
 import "./App.css";
 import AddButton from "./components/AddButton";
-import { API_KEY, API_URL, BASE64_IMAGE_HEADER } from "./Constants";
+import useImageUpload from "./hooks/useImageUpload";
 
 const App = () => {
-  const [result, setResult] = useState<string | null>(null);
-
-  const uploadImageToServer = (file: File) => {
-    loadImage(file, {
-      maxWidth: 400,
-      maxHeight: 400,
-      canvas: true,
-    })
-      .then(async (imageData: LoadImageResult) => {
-        const image = imageData.image as HTMLCanvasElement;
-
-        const imageBase64 = image.toDataURL("image/png");
-        const imageBase64Data = imageBase64.replace(BASE64_IMAGE_HEADER, "");
-        const data = {
-          image_file_b64: imageBase64Data,
-        };
-        const response = await fetch(API_URL, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-            "x-api-key": API_KEY,
-          },
-          body: JSON.stringify(data),
-        });
-
-        if (response.status >= 400 && response.status < 600) {
-          throw new Error("Bad response from server");
-        }
-
-        const result = await response.json();
-        const base64Result = BASE64_IMAGE_HEADER + result.result_b64;
-
-        setResult(base64Result);
-      })
-
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
-  const onImageAdd = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      uploadImageToServer(e.target.files[0]);
-    } else {
-      console.error("No file was picked");
-    }
-  }, []);
+  const { result, onImageAdd } = useImageUpload();
 
   return (
     <div>
       <header>
-        {!result && <AddButton onImageAdd={onImageAdd} />}
-        {!!result && <img alt="result from the API" src={result} width={300} />}
+        {!result ? (
+          <AddButton onImageAdd={onImageAdd} />
+        ) : (
+          <img alt="result from the API" src={result} width={300} />
+        )}
       </header>
     </div>
   );
---
+++ src/hooks/useImageUpload.ts
@@ -0,0 +1,59 @@
+import loadImage, { LoadImageResult } from "blueimp-load-image";
+import { ChangeEvent, useCallback, useState } from "react";
+import { API_KEY, API_URL, BASE64_IMAGE_HEADER } from "../Constants";
+
+const useImageUpload = () => {
+  const [result, setResult] = useState<string | null>(null);
+
+  const uploadImageToServer = (file: File) => {
+    loadImage(file, {
+      maxWidth: 400,
+      maxHeight: 400,
+      canvas: true,
+    })
+      .then(async (imageData: LoadImageResult) => {
+        const image = imageData.image as HTMLCanvasElement;
+        const imageBase64 = image.toDataURL("image/png");
+        const imageBase64Data = imageBase64.replace(BASE64_IMAGE_HEADER, "");
+        const data = {
+          image_file_b64: imageBase64Data,
+        };
+
+        // API call
+        const response = await fetch(API_URL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            "x-api-key": API_KEY,
+          },
+          body: JSON.stringify(data),
+        });
+
+        if (response.status >= 400 && response.status < 600) {
+          throw new Error("Bad response from server");
+        }
+
+        const result = await response.json();
+        const base64Result = BASE64_IMAGE_HEADER + result.result_b64;
+
+        setResult(base64Result);
+      })
+
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const onImageAdd = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      uploadImageToServer(e.target.files[0]);
+    } else {
+      console.error("No file was picked");
+    }
+  }, []);
+
+  return { result, onImageAdd };
+};
+
+export default useImageUpload;
