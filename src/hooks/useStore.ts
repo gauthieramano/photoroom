0bca1e0623fedd33c013d2801c5eb9ef76e7491f
Gauthier Amano <gauthier.net@gmail.com>
2022-08-11 04:57:22 +0200

    feat(app): ✨ use `localforage` for data persistence

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,8 +1,32 @@
-import { useState } from "react";
+import localforage from "localforage";
+import { useEffect, useState } from "react";
 import { Folders } from "../utils";
 
 const useStore = () => {
-  const [folders, setFolders] = useState<Folders>({});
+  // eslint-disable-next-line react/hook-use-state
+  const [folders, setAppFolders] = useState<Folders>({});
+
+  const setFolders = (nextFolders: Folders) => {
+    setAppFolders(nextFolders);
+
+    localforage.setItem("folders", nextFolders);
+  };
+
+  useEffect(() => {
+    const readLocalStorage = async () => {
+      try {
+        const nextFolders = (await localforage.getItem("folders")) as Folders;
+
+        if (nextFolders) {
+          setAppFolders(nextFolders);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    readLocalStorage();
+  }, []);
 
   return { folders, setFolders };
 };
