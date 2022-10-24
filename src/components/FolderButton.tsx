983199931aa6394163564c197b5283218072b38a
Gauthier Amano <gauthier.net@gmail.com>
2022-10-24 15:48:07 +0200

    feat(components): set text to `Add the new folder`

--- src/components/FolderButton.tsx
+++ src/components/FolderButton.tsx
@@ -10,7 +10,7 @@ const FolderButton = ({ onClick }: Props) => (
   <div>
     <p>{TITLE}</p>
     <button className="add-button-label" onClick={onClick} type="button">
-      <img alt="Add a new folder" className="add-button-image" src={start} />
+      <img alt="Add the new folder" className="add-button-image" src={start} />
     </button>
   </div>
 );
